25f7e525da553da93062a82527836ff7acf13cd2
cataraubianca <79210611+cataraubianca@users.noreply.github.com>
2024-08-15 17:31:47 +0200

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -2,14 +2,17 @@ from kafka import KafkaConsumer, TopicPartition
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 from datetime import datetime
+
 
 messages = []
 
 consumer = KafkaConsumer(
     'training1600000processednoemoticon',
     bootstrap_servers='localhost:9092',
-    auto_offset_reset='earliest'
+    auto_offset_reset='earliest',
+    consumer_timeout_ms=2000000
 )
 
 partitions = consumer.partitions_for_topic('training1600000processednoemoticon')
@@ -19,41 +22,39 @@ if partitions:
         consumer.seek_to_end(tp)
 
 def plot_arrival_times_unix(arrival_times_unix):
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(10, 6))
 
     counts = arrival_times_unix.value_counts().sort_index()
 
-    x_values = range(len(counts))
-
-    plt.bar(x_values, counts.values, color='lightcoral', edgecolor='black', width=0.8)
+    counts.plot(kind='bar', color='skyblue', edgecolor='black')
 
     plt.title('Counts of Message Arrival Times')
-    plt.xlabel('Messages')
+    plt.xlabel('Time')
     plt.ylabel('Count')
     plt.xticks(rotation=45)
     plt.grid(True)
 
-    first_timestamp = counts.index[0]
-    normalized_timestamps = counts.index - first_timestamp
-    plt.xticks(ticks=x_values[::max(1, len(x_values) // 10)], 
-               labels=[f"+{ts} ms" for ts in normalized_timestamps[::max(1, len(x_values) // 10)]])
+    step = max(1, len(counts) // 10)
+    plt.xticks(ticks=range(0, len(counts), step),
+               labels=[datetime.fromtimestamp(counts.index[i]).strftime('%Y-%m-%d %H:%M:%S')
+                       for i in range(0, len(counts), step)])
 
     plt.tight_layout()
     plt.show()
 
-
-
 try:
+    print("Starting to consume messages...")
     for message in consumer:
-        data = json.loads(message.value.decode('utf-8'))
+        data = json.loads(message.value.decode('utf-8'))  
+        print(data)
 
         if 'end_of_transmission' in data:
             print("End of transmission signal received.")
             break
 
         current_time = datetime.now()
-        current_time_milliseconds = int(current_time.timestamp() * 1000)
-        messages.append({'arrival_time_unix': current_time_milliseconds, **data})
+        current_time_unix = int(current_time.timestamp())
+        messages.append({'arrival_time_unix': current_time_unix, **data})
 finally:
     consumer.close()
 
